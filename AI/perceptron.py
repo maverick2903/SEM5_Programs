7aa642c2acab779ccde670c3b93113ceae18af5d
Varun Viswanath <varunvis2903@gmail.com>
2023-12-12 16:49:35 +0530

    Update perceptron weights and bias

--- AI/perceptron.py
+++ AI/perceptron.py
@@ -3,23 +3,24 @@ import numpy as np
 
 class Perceptron:
     def __init__(self, input_size, learning_rate=0.1, epochs=100):
-        self.weights = np.zeros(input_size + 1)
+        self.weights = np.random.rand(input_size)
         self.learning_rate = learning_rate
         self.epochs = epochs
+        self.bias = np.random.rand(1)
     
     def activate(self, x):
         return 1 if x >= 0 else 0
     
     def predict(self, inputs):
-        summation = np.dot(inputs, self.weights[1:]) + self.weights[0]
+        summation = np.dot(inputs, self.weights) + self.bias
         return self.activate(summation)
     
     def train(self, training_data, labels):
         for _ in range(self.epochs):
             for inputs, label in zip(training_data, labels):
                 prediction = self.predict(inputs)
-                self.weights[1:] += self.learning_rate * (label - prediction) * inputs
-                self.weights[0] += self.learning_rate * (label - prediction)
+                self.weights += self.learning_rate * (label - prediction) * inputs
+                self.bias += self.learning_rate * (label - prediction)
 
 def main():
     # AND gate training data
