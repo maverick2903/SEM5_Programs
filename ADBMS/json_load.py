ac0a547381290e385632e6a56140ea1499638242
Varun Viswanath <varunvis2903@gmail.com>
2023-12-15 06:37:03 +0530

    Add CRUD operations for user data in JSON file

--- ADBMS/json_load.py
+++ ADBMS/json_load.py
@@ -1,58 +1,43 @@
-
 import json
 from pprint import pprint
 
-def load_data():
-    with open('mobiledata.json', 'r') as file:
-        data = json.load(file)
-    return data
+with open('test.json','r') as file:
+    data = json.load(file)
 
-def save_data(data):
-    with open('mobiledata.json', 'w') as file:
-        json.dump(data, file, indent=2)
+pprint(data)
+print()
 
-def insert_user(data, user):
-    data['users'].append(user)
-    save_data(data)
+print("Query")
+query_id = 4
+for i in range(len(data)):
+    if i == query_id:
+        print(data[i])
 
-def delete_user(data, user_id):
-    deleted_user = next((user for user in data['users'] if user['id'] == user_id), None)
-    data['users'] = [user for user in data['users'] if user['id'] != user_id]
-    save_data(data)
-    return deleted_user
+print()
 
-def query_user(data, user_id):
-    user = next((user for user in data['users'] if user['id'] == user_id), None)
-    return user
+print("Insert")
+user = {'country': 'India', 'location': 'Delhi', 'name': 'Varun'}
+data.append(user)
+pprint(data)
 
-def alter_user(data, user_id, new_data):
-    user_index = next((index for index, user in enumerate(data['users']) if user['id'] == user_id), None)
-    if user_index is not None:
-        data['users'][user_index].update(new_data)
-        save_data(data)
-        return data['users'][user_index]
-    else:
-        print(f"User with ID {user_id} not found.")
+print()
 
-# Example usage:
-data = load_data()
+print("Delete")
+delete_id = 3
+for i in range(len(data)):
+    if i == delete_id:
+        data.remove(data[i])
+        break
 
-# Delete user with ID 3
-deleted_user = delete_user(data, 3)
-print("Deleted User:")
-pprint(deleted_user)
+pprint(data)
 
-# Query user with ID 4
-queried_user = query_user(data, 4)
-print("\nQueried User:")
-pprint(queried_user['email'])
+print()
 
-alter_data = {
-    "age": 26,
-    "email": "john.doe@example.com"
-}
-print("\nAltered data:", alter_user(data, 1, alter_data))
+print("Update")
+update_id = 1
+update_json = {'country': 'USA', 'location': 'New York', 'name': 'Varun'}
+for i in range(len(data)):
+    if i == update_id:
+        data[i].update(update_json)
 
-# Pretty print the entire data
-print("\nUpdated User Data:")
 pprint(data)
